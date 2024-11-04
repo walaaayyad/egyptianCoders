ba8d2597bbd618f1f8aaf280dde0403a80c1fc0a
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2024-11-04 19:18:51 +0200

    node -v 20

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import ViteComponents from 'vite-plugin-components';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteComponents({
+      exclude: [
+        /[\\/]node_modules[\\/]/,
+        /[\\/]\.git[\\/]/,
+      ],
+    })
+  ],
   base: "/egyptianCoders/"
-})
+});

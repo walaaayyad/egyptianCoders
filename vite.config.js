e7257e63490e1c671355262f1d8e959fec6e92d6
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2024-11-04 19:25:06 +0200

    hard key

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ViteComponents from 'vite-plugin-components';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteComponents({
-      exclude: [
-        /[\\/]node_modules[\\/]/,
-        /[\\/]\.git[\\/]/,
-      ],
-    })
-  ],
+  plugins: [react()],
   base: "/egyptianCoders/"
 });

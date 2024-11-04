9a0095703bbd49400afc2e7bbe035724bd825cf5
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2024-11-04 14:56:42 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteComponents from 'vite-plugin-components';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-    ViteComponents({
-                exclude: [
-                    /[\\/]node_modules[\\/](?!VITE_YOUTUBE_API_KEY)/,
-                    /[\\/]\.git[\\/]/,
-                ],
-		})
+    ViteComponents()
   ],
   base: "/egyptianCoders/"
 })

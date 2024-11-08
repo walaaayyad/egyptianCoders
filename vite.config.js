375c7486e373fc0fa2daec0327175bea2b27d428
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2024-11-08 08:18:30 +0200

    api issue

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/egyptianCoders/",
-  define: {
-    'process.env.VITE_YOUTUBE_API_KEY': JSON.stringify(process.env.VITE_YOUTUBE_API_KEY),
-  }
+  base: "/egyptianCoders/", 
 });

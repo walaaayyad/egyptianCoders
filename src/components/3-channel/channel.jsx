abc654f15e0b725f79d0737dcc45aa76cd5c3e64
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2024-11-15 09:48:39 +0200

    add responsive

--- src/components/3-channel/channel.jsx
+++ src/components/3-channel/channel.jsx
@@ -7,10 +7,7 @@ function YouTubeChannels() {
 
   // Use the environment variable for API key
   const apiKey = import.meta.env.VITE_YOUTUBE_API_KEY;
-  console.log('Api test',import.meta.env.VITE_YOUTUBE_API_KEY);
-
-
-  const channelIds = [
+  const data = [
     'UCveX_0uBOHVHbpV838OGXVA',
     'UCSNkfKl4cU-55Nm-ovsvOHQ',
     'UCvqYqoAu_Yp2v3hOBhi6qiQ',
@@ -34,13 +31,34 @@ function YouTubeChannels() {
     'UCPpguAKsCona6eJBjWT-2Cw',
     'UC2Gl0VkJjuMZh2jUkW6GI7Q',
     'UCiOouGMUwmtrH94zYUnlOMQ',
-    'UCNMvR21W292doQIU326HQTQ'
-  ].join(','); 
+    'UCNMvR21W292doQIU326HQTQ',
+    'UCywDxXiDYW3fDsxN1CtFvEw',
+    'UC4Hz-ntDHefICIBkutpadBA',
+    'UCtk1PX6rwD1A4yBsfXTVCcw',
+    'UC0bLYqDH3v-4LgqqzJp9e2Q',
+    'UCh-Wyi7aWSkvGujc-5FKc4w',
+    'UC-TeQlDX3eBHHXvzCb7DH9g',
+    'UCo0qWmxZdc1FhAL5emRNzcA',
+    'UChCMIwQrelEqLZT7Lt1L5Og',
+    'UCGn0UnpR7wbIzSXaKARH2dA',
+    'UCqzsLuqrvc-tQQ28fV5SZNQ',
+    'UCtOyb8800Av9Q-7i-l2mYyA',
+    'UCZiV7heCuc_307kI9Ze_7Mw',
+    'UCV_YCp6YEC74RbgLn3ninag',
+    'UCuJlicMvOtLYLK8pe2d2jbQ',
+    'UCN6CFzqrkiEfC93rjywpAhQ',
+    'UCEwfTBslCkubj_TlTBLNyNA',
+    'UCoqTiDGxa7C7CSDDq8K7_7A',
+    'UCUGC4ks3qfq5fLN8ElzuKhA',
+    'UCNpJQYA7qh3PyAgMUrA4oxw'
+  ]
+  const channelIds = data.join(','); 
+
+  console.log(data.length);
 
   useEffect(() => {
     const fetchChannels = async () => {
-      const apiUrl = `https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails&id=${channelIds}&key=${import.meta.env.VITE_YOUTUBE_API_KEY.replace(/^['"]|['"]$/g, '')}`;
-      console.log("API URL:", apiUrl); 
+      const apiUrl = `https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails&id=${channelIds}&key=${apiKey.replace(/^['"]|['"]$/g, '')}`;
       try {
         // Using fetch instead of axios
         const response = await fetch(apiUrl);

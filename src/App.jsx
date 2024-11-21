8a147124db9350ec89ed821a4f08953c040cb8ac
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2024-11-21 18:24:21 +0200

    Add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/1-header/header"
 import Hero from "./components/2-hero/hero"
 import YouTubeChannels from "./components/3-channel/channel"
+import Footer from "./components/4-footer/footer"
 
 function App() {
    
@@ -9,6 +10,7 @@ function App() {
             <Header/>
             <Hero/>
             <YouTubeChannels/>
+            <Footer/>
         </>
     )
 }
---
+++ src/components/4-footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = ()=> {
+  return (
+    <div className='footer flex'>
+      <div>Developed By Walaa Ayyad &copy; 2024</div>
+      <div className="footer-social-links flex">
+       <a href="https://github.com/walaaayyad/egyptianCoders" target="_blank"><span className='icon-github'></span></a>
+       <a href="https://www.linkedin.com/in/walaa-ayyad/" target="_blank"><span className='icon-linkedin-square'></span></a>
+       <a href="mailto:walaa_ayyad7@yahoo.com" target="_blank"><span className='icon-mail'></span></a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer

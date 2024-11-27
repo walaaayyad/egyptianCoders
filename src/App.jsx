9d399625cd273111cbaf28f4ec46178a31c2783b
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2024-11-27 12:47:24 +0200

    add scroll up btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import Header from "./components/1-header/header"
 import Hero from "./components/2-hero/hero"
 import YouTubeChannels from "./components/3-channel/channel"
-import Footer from "./components/4-footer/footer"
+import Footer from "./components/4-footer/footer";
+import { useState, useEffect} from "react";
 
 function App() {
-   
+const [scrollUp, setScrollUp] = useState(false); 
+
+//-------------------------- Scroll Up Button ---------------------------
+useEffect(()=> {
+  window.addEventListener("scroll", ()=> {
+    window.scrollY > 600 ? setScrollUp(true) : setScrollUp(false); 
+  })
+})
+
     return (
         <>
             <Header/>
             <Hero/>
             <YouTubeChannels/>
+            {/* Add condition to check if the user scroll down so the button appear by change its opacity */}
+            <a href="#hero" style={{opacity: scrollUp ? 1 : 0, transition: "1s"}}>
+            <button className="scrollUp-btn flex"><img src="./images/up.png" alt="" /></button> {/*https://www.freepik.com/icon/triangle_6844773#fromView=search&page=8&position=94&uuid=520a6f8f-869e-426a-8fda-056f8d31657c */}
+            </a>
             <Footer/>
         </>
     )

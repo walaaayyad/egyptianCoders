6a28457485178bd45c47bcbf9a069f2fcfd7355d
Walaa Ayyad <walaa_ayyad7@yahoo.com>
2024-12-26 01:25:20 +0200

    add vite-plugin-pwa  plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';  
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/egyptianCoders/", 
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{html,js,css,png,jpg,svg}'],
+      },
+      manifest: {
+        name: 'Egyptian Coders',
+        short_name: 'React PWA',
+        description: 'An Egyptian Coders React Progressive Web App',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/egyptianCoders/",
 });

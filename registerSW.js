c1a0ab2c35ae4694bc76339723098878ccfbc362
walaaayyad <walaaayyad@users.noreply.github.com>
2024-12-31 16:52:31 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/egyptianCoders/sw.js', { scope: '/egyptianCoders/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
